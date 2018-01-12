dd8eb9148a6491dc2fd7efcb20186f3b88d063d6
hi-the-swan <jason.portenoy@gmail.com>
2018-01-12 09:39:55 -0800

    successfully used python-igraph to get shortest path between two nodes

--- test_shortest_path_length.py
+++ test_shortest_path_length.py
@@ -14,30 +14,42 @@ logging.basicConfig(format='%(asctime)s %(name)s.%(lineno)d %(levelname)s : %(me
 # logger = logging.getLogger(__name__)
 logger = logging.getLogger('__main__').getChild(__name__)
 
-import networkx as nx
+import igraph
 
-def load_graph(fname, delimiter='\t', directed=False, nodetype=int):
-    if directed is True:
-        G = nx.DiGraph()
-    else:
-        G = nx.Graph()
-    G = nx.read_edgelist(fname, delimiter=delimiter, create_using=G, nodetype=nodetype)
+def load_graph(fname):
+    G = igraph.Graph.Read_Pajek(fname)
     return G
+
+def get_vertex_seq_id(G, original_id):
+    """gets the igraph vertex sequence id for a given node id"""
+    return G.vs.find(id=str(original_id)).index
 
 
 def main(args):
-    fname = os.path.abspath(args.edgelist)
+    fname = os.path.abspath(args.input)
     start = timer()
     logger.debug("loading graph from file: {}".format(fname))
-    G = load_graph(fname, delimiter=args.sep, directed=False, nodetype=int)
+    # G = load_graph(fname, delimiter=args.sep, directed=False, nodetype=int)
+    G = load_graph(fname)
     logger.debug ("done. took {}".format(format_timespan(timer()-start)))
 
     start = timer()
-    logger.debug("getting shortest path betweeen papers: {} -- {}".format(args.id1, args.id2))
-    sp_length = nx.shortest_path_length(G, source=args.id1, target=args.id2)
+    logger.debug("getting shortest path length betweeen papers: {} -- {}".format(args.id1, args.id2))
+    igraph_id1 = get_vertex_seq_id(G, args.id1)
+    igraph_id2 = get_vertex_seq_id(G, args.id2)
+    logger.debug("the igraph vertex sequence ids for these papers are: {} -- {}".format(igraph_id1, igraph_id2))
+    sp_length = G.shortest_paths(source=igraph_id1, target=igraph_id2, mode='ALL')
+    # mode='ALL' means consider the graph as undirected
     logger.debug("done. took {}".format(format_timespan(timer()-start)))
 
     print("Shortest path length between papers {} and {}: {}".format(args.id1, args.id2, sp_length))
+
+    start = timer()
+    logger.debug("getting shortest path betweeen papers: {} -- {}".format(args.id1, args.id2))
+    sp_path = G.get_shortest_paths(igraph_id1, igraph_id2, mode='ALL')
+    # mode='ALL' means consider the graph as undirected
+    logger.debug("done. took {}".format(format_timespan(timer()-start)))
+    print("Shortest path between papers {} and {}: {}".format(args.id1, args.id2, sp_path))
 
 if __name__ == "__main__":
     total_start = timer()
@@ -45,11 +57,11 @@ if __name__ == "__main__":
     logger.info(" ".join(sys.argv))
     logger.info( '{:%Y-%m-%d %H:%M:%S}'.format(datetime.now()) )
     import argparse
-    parser = argparse.ArgumentParser(description="test shortest path length. takes as input the full edgelist, then the paper ids of two papers to find the shortest path length between")
-    parser.add_argument("edgelist", help="file containing the edgelist")
+    parser = argparse.ArgumentParser(description="test shortest path length. takes as input the full network (as a pajek file), then the paper ids of two papers to find the shortest path length between")
+    parser.add_argument("input", help="file containing the network (.net pajek file)")
     parser.add_argument("id1", type=int, help="first paper id (integer)")
     parser.add_argument("id2", type=int, help="second paper id (integer)")
-    parser.add_argument("--sep", default='\t', help="delimiter for the edgelist file (default: tab)")
+    # parser.add_argument("--sep", default='\t', help="delimiter for the edgelist file (default: tab)")
     parser.add_argument("--debug", action='store_true', help="output debugging info")
     global args
     args = parser.parse_args()
